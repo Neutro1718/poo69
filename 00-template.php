b624b349311daefe79ebbec413dcde6ed5ea2837
Santiago Bonilla <santiagobonilla13579@gmail.com>
2023-02-14 12:11:00 -0500

    Feed: we use classes and fuctions

---
+++ 00-template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="min-h-[100vh] flex justify-center items-center text-gray-800">
+    <main class="w-[380px] h-[600px] bg-white/20 rounded">
+        <header class="
+        flex 
+        gap-5
+        justify-center
+        items-center
+        border-b
+        pb-5
+        border-dashed">
+
+        <a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-10 h-10 hover:-translate-x-2 transition hover:text-red-400 hover:scale-140 text-red-700 ">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </a>
+      
+            <h1 class="text-2xl">Main Mesnu</h1>
+        </header>
+       
+    </main>
+    <script src="js/tailwind-3.2.4.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="min-h-[100vh] flex justify-center items-center text-gray-800">
+    <main class="w-[380px] h-[600px] bg-white/20 rounded">
+        <header class="
+        flex 
+        gap-5
+        justify-center
+        items-center
+        border-b
+        pb-5
+        border-dashed">
+
+        
+      
+            <h1 class="text-2xl">Main Menu</h1>
+        </header>
+        <section class="min h-[515px] overflow-y-auto m-4">
+        <nav class="flex flex-col gap-4 items-center">
+            <a href="01-class.php" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">01 - Class</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">02 - Construct </a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">03 - Private</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">04 - Collaboration</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">05 - Parameters</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">06 - Extends</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">07 - Overwrite Overwrite Method</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">08 - Overwrite Construct </a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">09 - Class Abstract </a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">10 - Method Abstract</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">11 - Class Final</a>
+            <a href="" class="bg-blue-800 px-4 py-2 text-white/80 rounded w-[200px] hover:scale-105 transition-all opacity-80 hover:opacity-100">12 - i like dickl</a>
+
+       
+        </nav>
+        </section>
+    </main>
+    <script src="js/tailwind-3.2.4.js"></script>
+</body>
+</html>
